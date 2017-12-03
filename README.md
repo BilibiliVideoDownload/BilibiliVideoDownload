3b946cfe2ab1c65b8b75681abf377a9146175064
blogwy <543668913@qq.com>
2017-12-03 15:31:47 +0800

    v1.2.0

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 ### BilibiliVideoDownload
 ----
-基于bootstrap + jq 的单网页，可以下载bilibili视频
+基于MDUI的单网页，可以下载单P和多P的bilibili视频的网站
 ### 版本
 ----
+v1.2.0 `2017-12-3`
+
+更换全新MDUI框架，添加视频背景，优化js代码
+
+MDUI 是一套用于开发 Material Design 网页的前端框架
+
 v1.1.3 `2017-12-1`
 
 修复获取视频信息失败，由于9bl.bakayun.cn网站维护导致api失效，现已使用bilibili官方api接口，更稳定
@@ -33,10 +39,9 @@ v1.0.0 `2017-10-21`
 ![图片1](https://raw.githubusercontent.com/blogwy/BilibiliVideoDownload/master/img/1.png)
 ![图片2](https://raw.githubusercontent.com/blogwy/BilibiliVideoDownload/master/img/2.png)
 ![图片3](https://raw.githubusercontent.com/blogwy/BilibiliVideoDownload/master/img/3.png)
-![图片4](https://raw.githubusercontent.com/blogwy/BilibiliVideoDownload/master/img/4.png)
 ### 演示地址
 ----
-[点击查看](http://bt720p.com/bilibili/)
+[点击查看](http://www.bilibibi.cn)
 ### 实现的功能
 ----
 1. 获取视频直链
@@ -53,12 +58,6 @@ v1.0.0 `2017-10-21`
 - http://api.bilibili.com//playurl?aid=AV号&page=分P号&platform=html5&quality=1&vtype=mp4&type=jsonp&callback=fn&token=d3bd9275f0f2cda07f2406740db06c5d
 ### 感谢
 ----
-1.⑨BiLiBiLi 第三方哔哩哔哩API提供平台
-
-[http://9bl.bakayun.cn/](http://9bl.bakayun.cn/)
-
-2.Images.weserv.nl图片缓存代理
+- Images.weserv.nl图片缓存代理
 
 [https://github.com/andrieslouw/imagesweserv](https://github.com/andrieslouw/imagesweserv)
-
-3.bs and jq
--- js/main.js
+++ js/main.js
@@ -1,133 +1,142 @@
+
 // 本程序由blogwy维护，程序免费开源所有人均可修改，切勿商业化
 // GitHub：https://github.com/blogwy/BilibiliVideoDownload
+// 个人网站欢迎访问 https://www.blogwy.com
 
-//初始化模态框
-    $('#myModal').on('shown.bs.modal', function () {
-        $('#myInput').focus();
-    });
-//关闭分p页    
-    $('#singlep').on('shown.bs.modal',function (e) {
-        $('#morep').modal('hide')
-    })
-// 复制链接,点击下载
-    var clipboard = new Clipboard('.btn');
+// 定义命名空间
+var $$ = mdui.JQ;
 
-    clipboard.on('success', function(e) {
-        console.log(e);
-        // window.open(e.text); 打开下载链接    
-    });
+// 复制下载链接
+var clipboard = new Clipboard('#ipt');
+clipboard.on('success', function(e) {
+    console.log(e.text); 
+});
 
-    clipboard.on('error', function(e) {
-        console.log(e);
-    });
-//获取数据
-// http://api.bilibili.com/view?type=jsonp&appkey=8e9fc618fbd41e28&id=16832727&page=1&callback=fn
-    $('#btn').on("click",function (e) {
-        e.preventDefault();
-        if ($('#aid').val()){
-            $.ajax({
-                type: 'get',
-                url: `http://api.bilibili.com//playurl?aid=${ $('#aid').val() }&page=2&platform=html5&quality=1&vtype=mp4&type=jsonp&callback=fn&token=d3bd9275f0f2cda07f2406740db06c5d`,
-                dataType: 'jsonp',
-                success: function (da) {
-                    if (da.durl){
-                        console.log('多p');
-                        $('#morep').modal('show');
-                        morep()
+// 初始化dialog
+var inst = new mdui.Dialog('#msgDialog');
 
-                    }else{
-                        console.log('单p');
-                        $('#singlep').modal('show');
-                        singlep()
-                    }
-                },
-                error : function () {
-                    alert('获取失败');
+// 监听首页点击事件
+document.getElementById('downloadbtn').addEventListener('click', function (e) {
+  e.preventDefault();
+  main();
+});
+
+function main() {
+  if ($$('#avnum').val()){
+    $$.ajax({
+      method: 'GET',
+      url: `http://api.bilibili.com//playurl?aid=${ $$('#avnum').val() }&page=2&platform=html5&quality=1&vtype=mp4&type=jsonp&callback=fn&token=d3bd9275f0f2cda07f2406740db06c5d`,
+      dataType: 'jsonp',
+      success: function (da) {
+        if (da.durl){
+          console.log('多p');
+          mdui.dialog({
+            title: '这是一个分P视频，请输入分P号',
+            content: '<div class="mdui-textfield"><input class="mdui-textfield-input" id="pnum" type="text"/></div>',
+            buttons: [
+              {
+                text: '取消'
+              },
+              {
+                text: '确认',
+                onClick: function(){
+                  var value = $$('#pnum').val();
+                  inst.open();
+                  morep(value);
                 }
-            });
+              }
+            ]
+          });
         }else{
-            alert('请输入av号')
+          console.log('单p');
+          inst.open();
+          singlep();
         }
+      },
+      error : function () {
+        mdui.alert('获取失败');
+      }
     });
-    function morep(){
-        $('#moreplink').on('click',function (e) {
-            e.preventDefault();
-            $.ajax({
-                type : 'get',
-                url : `http://api.bilibili.com/view?type=jsonp&appkey=8e9fc618fbd41e28&id=${ $('#aid').val() }&page=1&callback=fn`,
-                dataType : 'jsonp',
-                success : function (da) {
-                    var UserId = da.mid,
-                        videotit = da.title,
-                        upname = da.author,
-                        videoimg = da.pic;
-                    var realimg = 'https://images.weserv.nl/?url=' + videoimg.replace(/https\:/,'') + '&w=218&h=135';
-                    var uplink = `https://space.bilibili.com/${ UserId }/#!/`;
-                    $('#videotit').html('[p' + $('#pcount').val() + ']' + videotit);
-                    $('#videoimgs').attr({src : realimg,alt : videotit});
-                    $('#upname').html(upname).attr({href : uplink});
-                    $.ajax({
-                        type : 'get',
-                        url : `http://api.bilibili.com//playurl?aid=${ $('#aid').val() }&page=${ $('#pcount').val() }&platform=html5&quality=1&vtype=mp4&type=jsonp&callback=fn&token=d3bd9275f0f2cda07f2406740db06c5d`,
-                        dataType : 'jsonp',
-                        success : function (data) {
-                            var maindownurl = data.durl[0].url,
-                                videotime = data.durl[0].length,
-                                format = data.format
-                                videosize = data.durl[0].size;
-                            $('#videosize').html((videosize/1048576).toFixed(2) + 'mb');
-                            $('#videotime').html((videotime/60000).toFixed(2) + 'min');
-                            $('#copytext').val(maindownurl);
-                            $('#videoformat').html(format.toUpperCase());
-                        },
-                        error : function () {
-                            alert('获取下载链接出错');
-                        }
-                    });
-                },
-                error : function () {
-                    alert('获取视频信息出错');
-                }
-            })
-        })
+  }else{
+    mdui.alert('请输入av号')
+  }
+}
+function morep(value) {
+  $$.ajax({
+    method : 'GET',
+    url : `http://api.bilibili.com/view?type=jsonp&appkey=8e9fc618fbd41e28&id=${ $$('#avnum').val() }&page=1&callback=fn`,
+    dataType : 'jsonp',
+    success : function (da) {
+      var UserId = da.mid,
+          videotit = da.title,
+          upname = da.author,
+          videoimg = da.pic;
+      var realimg = 'https://images.weserv.nl/?url=' + videoimg.replace(/https\:/,'') + '&w=218&h=135';
+      var uplink = `https://space.bilibili.com/${ UserId }/#!/`;
+      $$('#videotit').html('[p' + value + ']' + videotit);
+      $$('#videoimgs').attr({src : realimg,alt : videotit});
+      $$('#upname').html(upname).attr({href : uplink});
+      $$.ajax({
+        type : 'get',
+        url : `http://api.bilibili.com//playurl?aid=${ $$('#avnum').val() }&page=${ value }&platform=html5&quality=1&vtype=mp4&type=jsonp&callback=fn&token=d3bd9275f0f2cda07f2406740db06c5d`,
+        dataType : 'jsonp',
+        success : function (data) {
+          var maindownurl = data.durl[0].url,
+              videotime = data.durl[0].length,
+              format = data.format,
+              videosize = data.durl[0].size;
+          $$('#videosize').html((videosize/1048576).toFixed(2) + 'mb');
+          $$('#videotime').html((videotime/60000).toFixed(2) + 'min');
+          $$('#ipt').val(maindownurl);
+          $$('#videoformat').html(format.toUpperCase());
+        },
+        error : function () {
+          mdui.alert('获取下载链接出错');
+        }
+      });
+    },
+    error : function () {
+      mdui.alert('获取视频信息出错');
     }
-    function singlep(){
-        $.ajax({
-            type : 'get',
-            url : `http://api.bilibili.com/view?type=jsonp&appkey=8e9fc618fbd41e28&id=${ $('#aid').val() }&page=1&callback=fn`,
-            dataType : 'jsonp',
-            success : function (da) {
-                var UserId = da.mid,
-                    videotit = da.title,
-                    upname = da.author,
-                    videoimg = da.pic;
-                var realimg = 'https://images.weserv.nl/?url=' + videoimg.replace(/https\:/,'') + '&w=218&h=135';
-                console.log(realimg);
-                var uplink = `https://space.bilibili.com/${ UserId }/#!/`;
-                $('#videotit').html('[p' + $('#pcount').val() + ']' + videotit);
-                $('#videoimgs').attr({src : realimg,alt : videotit});
-                $('#upname').html(upname).attr({href : uplink});
-                $.ajax({
-                    type : 'get',
-                    url : `http://api.bilibili.com//playurl?aid=${ $('#aid').val() }&page=1&platform=html5&quality=1&vtype=mp4&type=jsonp&callback=fn&token=d3bd9275f0f2cda07f2406740db06c5d`,
-                    dataType : 'jsonp',
-                    success : function (data) {
-                        var maindownurl = data.durl[0].url,
-                            videotime = data.durl[0].length,
-                            format = data.format
-                            videosize = data.durl[0].size;
-                        $('#videosize').html((videosize/1048576).toFixed(2) + 'mb');
-                        $('#videotime').html((videotime/60000).toFixed(2) + 'min');
-                        $('#copytext').val(maindownurl);
-                        $('#videoformat').html(format.toUpperCase());
-                    },
-                    error : function () {
-                        alert('获取下载链接出错');
-                    }
-                });
-            },
-            error : function () {
-                alert('获取视频信息出错');
-            }
-        })
+  })
+}
+
+function singlep() {
+  $$.ajax({
+    method : 'GET',
+    url : `http://api.bilibili.com/view?type=jsonp&appkey=8e9fc618fbd41e28&id=${ $$('#avnum').val() }&page=1&callback=fn`,
+    dataType : 'jsonp',
+    success : function (da) {
+      var UserId = da.mid,
+          videotit = da.title,
+          upname = da.author,
+          videoimg = da.pic;
+      var realimg = 'https://images.weserv.nl/?url=' + videoimg.replace(/https\:/,'') + '&w=218&h=135';
+      var uplink = `https://space.bilibili.com/${ UserId }/#!/`;
+      $$('#videotit').html(videotit);
+      $$('#videoimgs').attr({src : realimg,alt : videotit});
+      $$('#upname').html(upname).attr({href : uplink});
+      $$.ajax({
+        type : 'get',
+        url : `http://api.bilibili.com//playurl?aid=${ $$('#avnum').val() }&page=1&platform=html5&quality=1&vtype=mp4&type=jsonp&callback=fn&token=d3bd9275f0f2cda07f2406740db06c5d`,
+        dataType : 'jsonp',
+        success : function (data) {
+          var maindownurl = data.durl[0].url,
+              videotime = data.durl[0].length,
+              format = data.format
+              videosize = data.durl[0].size;
+          $$('#videosize').html((videosize/1048576).toFixed(2) + 'mb');
+          $$('#videotime').html((videotime/60000).toFixed(2) + 'min');
+          $$('#ipt').val(maindownurl);
+          $$('#videoformat').html(format.toUpperCase());
+        },
+        error : function () {
+          mdui.alert('获取下载链接出错');
+        }
+      });
+    },
+    error : function () {
+      mdui.alert('获取视频信息出错');
     }
+  })
+}
